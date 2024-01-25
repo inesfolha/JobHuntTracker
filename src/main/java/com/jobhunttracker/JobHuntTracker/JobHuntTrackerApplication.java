b5f49ae8e559188e6634ab519afb28ae9f00a02c
inesfolha <ines.folha98@gmail.com>
2024-01-25 16:41:24 +0000

    logging config added

--- src/main/java/com/jobhunttracker/JobHuntTracker/JobHuntTrackerApplication.java
+++ src/main/java/com/jobhunttracker/JobHuntTracker/JobHuntTrackerApplication.java
@@ -1,13 +1,27 @@
 package com.jobhunttracker.JobHuntTracker;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-@SpringBootApplication @EnableMongoRepositories("com.jobhunttracker.JobHuntTracker.repository")
-public class JobHuntTrackerApplication {
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+@SpringBootApplication @EnableMongoRepositories("com.jobhunttracker.JobHuntTracker.repository") @Slf4j
+public class JobHuntTrackerApplication {
 	public static void main(String[] args) {
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		System.setProperty("timestamp", dateFormat.format(new Date()));
+
+		log.info("Starting JobHuntTrackerApplication...");
+		try {
 		SpringApplication.run(JobHuntTrackerApplication.class, args);
+		log.info("JobHuntTrackerApplication started successfully.");
+		} catch (Exception e) {
+			log.error("Error occurred during application startup:", e);
+			System.out.println(("Error occurred during application startup:" + e));
+		}
 	}
 
 
--- src/main/java/com/jobhunttracker/JobHuntTracker/service/UserService.java
+++ src/main/java/com/jobhunttracker/JobHuntTracker/service/UserService.java
@@ -3,6 +3,7 @@ package com.jobhunttracker.JobHuntTracker.service;
 import com.jobhunttracker.JobHuntTracker.entity.User;
 import com.jobhunttracker.JobHuntTracker.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,15 @@ public class UserService {
     UserRepository userRepository;
 
     public User createUser(String username, String email, String password) {
-        User user = new User(username, email, password);
-        return userRepository.save(user);
+        try {
+            User user = new User(username, email, password);
+            return userRepository.save(user);
+            //logger.info("User Successfully created");
+        } catch (DuplicateKeyException e) {
+            // Handle the case where a user with the same email already exists
+            throw new RuntimeException("Email already exists");
+            //logger.error("Error creating account: Email already exists");
+        }
     }
 }
+
